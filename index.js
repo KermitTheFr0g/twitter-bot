2d1115bf68417aac16d6e0b1916b2e34c9dfc951
Oli Gray <oligray26@gmail.com>
2021-09-28 14:23:19 +0100

    comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const client = require('./twit');
 
-
+// very quick way to reverse a string
 function reverseString(text){
     let splitString = text.split('');
 
@@ -11,16 +11,20 @@ function reverseString(text){
     return joined
 }
 
-let stream = client.stream('statuses/filter', { follow: '1090372176606347265'})
 
+// defines the stream watching for a status from specific user
+let stream = client.stream('statuses/filter', { follow: '{USER ID OF THE ACCOUNT TO WATCH}'})
+
+// when a tweet is received this function is run
 stream.on('tweet', async function(tweet) {
 
+    // checks to see if the tweet is a reply
     if(tweet.in_reply_to_status_id == null){
-        const {data} = await client.post('statuses/update', { in_reply_to_status_id: tweet.id_str, status: '@' + tweet.user.screen_name + ' ' + reverseString(tweet.text)})
-        console.log('Replied with - ' + reverseString(tweet.text))
+        let reversedText = reverseString(tweet.text)
+
+        const {data} = await client.post('statuses/update', { in_reply_to_status_id: tweet.id_str, status: '@' + tweet.user.screen_name + ' ' + reversedText})
+        console.log('Replied with - ' + reversedText)
     }
-
-
 
 })
 
